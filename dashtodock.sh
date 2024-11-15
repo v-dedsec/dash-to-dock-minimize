fb748d3185182c88436ec307e7d67a67bec2ea71
Vishnu.P <110442683+v-dedsec@users.noreply.github.com>
2024-11-15 16:13:34 +0530

    Update dashtodock.sh

--- dashtodock.sh
+++ dashtodock.sh
@@ -1,1 +1,7 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+
+
+
+
+sudo apt purge -y i3 i3-wm i3status i3lock dmenu awesome awesome-extra && sudo apt autoremove --purge -y && rm -rf ~/.config/{i3,i3status,awesome} && find ~ -name "*i3*" -o -name "*awesome*" | xargs rm -rf
+
